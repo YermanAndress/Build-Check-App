2cab3c9c75213e7b23a3a29a83442b087bcd4ef3
Kevin <kevin.montes03@uceva.edu.co>
2026-04-05 17:06:05 -0500

    BC-40 feature: Recordar cambiar la URL base dependiendo de la ip de la persona

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,9 @@ class ApiConfig {
   // │  Emulador Android (AVD)     → http://10.0.2.2:8080/api        │
   // │  Dispositivo físico         → http://192.168.101.6:8080/api   │
   // └─────────────────────────────────────────────────────────────────┘
-  static const String baseUrl     = 'http://localhost:8080/api'; // ← WEB (Edge/Chrome)
+  // static const String baseUrl     = 'http://localhost:8080/api'; // ← WEB (Edge/Chrome)
   // static const String baseUrl  = 'http://10.0.2.2:8080/api';      // ← Emulador Android
-  // static const String baseUrl  = 'http://192.168.101.6:8080/api'; // ← Dispositivo físico
+  static const String baseUrl = 'http://192.168.101.5:8080/api'; // ← Dispositivo físico
   static const String movimientos = '$baseUrl/movimientos-service/movimientos';
   static const String materiales  = '$baseUrl/materiales-service/materiales';
   static const String facturas    = '$baseUrl/facturas-service/facturas';
