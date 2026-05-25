c4419458b2be9efc070b2d0320f4eee2a874abe7
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-05-25 09:38:07 -0500

    Lo de ia

--- lib/services/auth_header.dart
+++ lib/services/auth_header.dart
@@ -4,6 +4,7 @@ import 'package:build_check_app/core/proyecto_actual.dart';
 class AuthHeader {
   static Future<Map<String, String>> getHeaders({bool json = true}) async {
     final token = await SecureStorage.read("accessToken");
+    print("token: $token");
     final headers = <String, String>{};
     if (token != null && token.isNotEmpty) {
       headers["Authorization"] = "Bearer $token";
